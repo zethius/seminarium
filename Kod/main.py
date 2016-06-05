f12cff5ff7f0fb3de08b43b992367239d878830f
peps <zethius@mat.umk.pl>
2016-06-05 13:01:28 +0200

    Export/Import JSON + Rescan

--- Kod/main.py
+++ Kod/main.py
@@ -1,59 +1,18 @@
+import Objects
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_string("""
-<MainScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Where is Mnemo?'
-        Button:
-            text: 'SETS'
-            background_color: [0, 1, 0, 1]
-            on_press: root.manager.current = 'sets'
-        Button:
-            text: 'TESTS'
-            background_color:[0, 0.75, 0, 1]
-            on_press: root.manager.current = 'tests'
-
-
-<SetsScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Sets'
-        Button:
-            text: 'Add'
-            background_color: [0, 0.8, 0, 1]
-        Button:
-            text: 'Remove'
-            background_color: [0.75, 0, 0, 1]
-        Button:
-            text: 'Back'
-            on_press: root.manager.current = 'main'
-<TestsScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Tests'
-        Button:
-            text: 'Add'
-            background_color: [0, 0.8, 0, 1]
-        Button:
-            text: 'Remove'
-            background_color: [0.75, 0, 0, 1]
-        Button:
-            text: 'Back'
-            on_press: root.manager.current = 'main'
-""")
-
+setsArray = Objects.rescan()
 
 class MainScreen(Screen):
     pass
 
 
 class SetsScreen(Screen):
+    setsNames = []
+    for set in setsArray:
+        setsNames.append(set.name)
     pass
 
 
@@ -61,17 +20,19 @@ class TestsScreen(Screen):
     pass
 
 
-sm = ScreenManager()
-sm.add_widget(MainScreen(name='main'))
-sm.add_widget(SetsScreen(name='sets'))
-sm.add_widget(TestsScreen(name='tests'))
-sm.current = 'main'
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("main.kv")
 
 
 class MainApp(App):
     def build(self):
-        return sm
+        return presentation
 
 
 if __name__ == '__main__':
+    setsArray[1].export()
+    # setsArray = Objects.rescan()
+    # print(setsArray[1].to_json())
     MainApp().run()
