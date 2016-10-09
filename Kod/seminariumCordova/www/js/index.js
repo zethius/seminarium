bfb0f355defac6389ef12f2894020760ebc1ba98
peps <zethius@mat.umk.pl>
2016-10-09 21:39:39 +0200

    icons

--- Kod/seminariumCordova/www/js/index.js
+++ Kod/seminariumCordova/www/js/index.js
@@ -10,81 +10,106 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('MainMenuSetsButton').addEventListener('click', setList.SetsMenu);
+        document.getElementById('MainMenuSetsButton').addEventListener('click', setList.initialize);
         
     },
-
-    
-
-   
-
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+ 
+    showMain: function(){
+        document.getElementById('MainMenuScreen').style.display='block';
+        document.getElementById('SetsMenuScreen').style.display='none';
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    onDeviceReady: function() {
+    },
 };
 
 
 var setList = {
-
+    sets: [],
     initialize: function() {
-        setList.renderTable();
+        var self = this;
+        document.getElementById('MainMenuScreen').style.display='none';
+        document.getElementById('SetsMenuScreen').style.display='block';
+        document.getElementById('SetsMenuBack').addEventListener('click', app.showMain);
+        setList.sets =  setList.iterateFolders();
+        console.log(setList.sets);
+
+        setTimeout(function(){  setList.renderTable();}, 10);
     },
 
     renderTable: function () {
         var body = document.getElementById('SetsMenuScreen');
-        console.log(body);
         var tbl = document.getElementById('SetsTable');
+        tbl.innerHTML="";
         tbl.className="SetsTable"
         var tbdy = document.createElement('tbody');
-        //sets = skanowanie folderu /resources/sets
-        //foreach set
 
-        for (var i = 0; i < 3; i++) {
+        setList.sets.forEach(function(entry) {
+            console.log(entry);
+        });
+
+
+        setList.sets.forEach(function(el){
             var tr = document.createElement('tr');
             var icon = document.createElement('td');
             icon.className="icon";
-            icon.innerHTML="icona";
+            icon.innerHTML = '<img src=\''+el.icon+'\'height="32" width="32">';
+            // icon.innerHTML=el.icon;
             icon.appendChild(document.createTextNode('\u0020'))
 
             var label = document.createElement('td');
             label.className="label";
-            label.innerHTML="nazwa zestawu";
+            label.innerHTML=el.name;
             label.appendChild(document.createTextNode('\u0020'))
             var button = document.createElement('td');
             button.className="button";
             button.innerHTML="X";
             button.appendChild(document.createTextNode('\u0020'))
-
             tr.appendChild(icon);
             tr.appendChild(label);
             tr.appendChild(button);
             tbdy.appendChild(tr);
-        }
+        });
+        // for (var i = 0; i < 3; i++) {
+        //     var tr = document.createElement('tr');
+        //     var icon = document.createElement('td');
+        //     icon.className="icon";
+        //     icon.innerHTML="icona";
+        //     icon.appendChild(document.createTextNode('\u0020'))
+
+        //     var label = document.createElement('td');
+        //     label.className="label";
+        //     label.innerHTML="nazwa zestawu";
+        //     label.appendChild(document.createTextNode('\u0020'))
+        //     var button = document.createElement('td');
+        //     button.className="button";
+        //     button.innerHTML="X";
+        //     button.appendChild(document.createTextNode('\u0020'))
+
+        //     tr.appendChild(icon);
+        //     tr.appendChild(label);
+        //     tr.appendChild(button);
+        //     tbdy.appendChild(tr);
+        // }
         tbl.appendChild(tbdy);
         body.appendChild(tbl)
     },
 
-    SetsMenu: function(event){
-        document.getElementById('MainMenuScreen').style.display='none';
-        document.getElementById('SetsMenuScreen').style.display='block';
-        setList.initialize();
-    },
+    iterateFolders: function(){
+        var set= [];
+        var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var obj = JSON.parse(this.response);
+                set.push(obj);
+            }
+          };
+          xhttp.open("GET", "resources/sets/fizyka/.info.json", true);
+          xhttp.send();
+          return set;
+    } 
+
+
 };
 
 app.initialize();
