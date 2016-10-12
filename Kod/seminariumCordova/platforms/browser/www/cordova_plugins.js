df6442cc251c35118bd8feef252a250a3aaa8708
zethius <zethius@mat.umk.pl>
2016-10-12 13:33:59 +0200

    osx

--- Kod/seminariumCordova/platforms/browser/www/cordova_plugins.js
+++ Kod/seminariumCordova/platforms/browser/www/cordova_plugins.js
@@ -192,39 +192,6 @@ module.exports = [
         "merges": [
             "window.FileSystem"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-        "id": "cordova-plugin-inappbrowser.inappbrowser",
-        "pluginId": "cordova-plugin-inappbrowser",
-        "clobbers": [
-            "cordova.InAppBrowser.open",
-            "window.open"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-inappbrowser/src/browser/InAppBrowserProxy.js",
-        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
-        "pluginId": "cordova-plugin-inappbrowser",
-        "merges": [
-            ""
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
-        "id": "cordova-plugin-dialogs.notification",
-        "pluginId": "cordova-plugin-dialogs",
-        "merges": [
-            "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
-        "id": "cordova-plugin-dialogs.notification_browser",
-        "pluginId": "cordova-plugin-dialogs",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -234,9 +201,7 @@ module.exports.metadata =
     "cordova-plugin-device": "1.1.3",
     "cordova-plugin-console": "1.0.4",
     "cordova-plugin-compat": "1.0.0",
-    "cordova-plugin-file": "4.3.0",
-    "cordova-plugin-inappbrowser": "1.5.0",
-    "cordova-plugin-dialogs": "1.3.0"
+    "cordova-plugin-file": "4.3.0"
 }
 // BOTTOM OF METADATA
 });
--- Kod/seminariumCordova/platforms/browser/www/js/index.js
+++ Kod/seminariumCordova/platforms/browser/www/js/index.js
@@ -1,9 +1,108 @@
+var tempData= [
+     {'filename':'fizyka/info.json', 'object':{
+                                                "cardSet": [
+                                                    {
+                                                        "back": "mc^2", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.2, 
+                                                        "front": "e=", 
+                                                        "id": 0
+                                                    }
+                                                ], 
+                                                "description": "", 
+                                                "difficulty": 0.2, 
+                                                "icon": "atom.png", 
+                                                "name": "fizyka", 
+                                                "path": "resources/sets/fizyka"
+                                            }
+    },
+     {'filename':'ludzie/info.json', 'object':{
+                                                "cardSet": [
+                                                    {
+                                                        "back": "Parzych", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.3, 
+                                                        "front": "Patrycja", 
+                                                        "id": 0
+                                                    }, 
+                                                    {
+                                                        "back": "Stalewski", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.3, 
+                                                        "front": "Patryk", 
+                                                        "id": 0
+                                                    }, 
+                                                    {
+                                                        "back": "Eli\u0144ski", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.4, 
+                                                        "front": "Remek", 
+                                                        "id": 0
+                                                    }, 
+                                                    {
+                                                        "back": "Stalewska", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.3, 
+                                                        "front": "Julia", 
+                                                        "id": 0
+                                                    }, 
+                                                    {
+                                                        "back": "Stalewski K.", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.1, 
+                                                        "front": "Kacper", 
+                                                        "id": 0
+                                                    }, 
+                                                    {
+                                                        "back": "Stalewski M.", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.2, 
+                                                        "front": "Mariusz", 
+                                                        "id": 0
+                                                    }, 
+                                                    {
+                                                        "back": "Parzych", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.3, 
+                                                        "front": "Patrycja", 
+                                                        "id": 0
+                                                    }
+                                                ], 
+                                                "description": "", 
+                                                "difficulty": 0.27142857142857146, 
+                                                "icon": "scientist.png", 
+                                                "name": "ludzie", 
+                                                "path": "resources/sets/ludzie"
+                                            }
+    },
+     {'filename':'matematyka/info.json', 'object':{
+                                                "cardSet": [
+                                                    {
+                                                        "back": "a^2+b^2 = c^2", 
+                                                        "color": "#92031", 
+                                                        "difficulty": 0.2, 
+                                                        "front": "Wz\u00f3r pitagorasa", 
+                                                        "id": 0
+                                                    }
+                                                ], 
+                                                "description": "", 
+                                                "difficulty": 0.2, 
+                                                "icon": "brain.png", 
+                                                "name": "matematyka", 
+                                                "path": "resources/sets/matematyka"
+                                            }
+    },
+];
+
+
 var app = {
     // Application Constructor
     initialize: function() {
         var self = this;
         this.bindEvents();
-
+        this.fs=null;
+        this.fsroot=null;
+        console.log(tempData);
        
     },
     // Bind Event Listeners
@@ -23,35 +122,113 @@ var app = {
     },
 
     
-    fail: function(e) {
-        console.log("FileSystem Error");
-        console.dir(e);
+    errorHandler: function (fileName, e) {  
+        var msg = '';
+
+        switch (e.code) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'Storage quota exceeded';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'File not found';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'Security error';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'Invalid modification';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'Invalid state';
+                break;
+            default:
+                msg = 'Unknown error';
+                break;
+        };
+
+        console.log('Error (' + fileName + '): ' + msg);
     },
 
     gotFile: function(fileEntry) {
+        console.log(fileEntry);
+        this.fs=fileEntry;
+        this.fsroot=fileEntry.root;
+        app.createFile("1.txt");
+        app.getFiles();
 
-        fileEntry.file(function(file) {
-            var reader = new FileReader();
+        // console.log(this);
+        // fileEntry.file(function(file) {
+        //     var reader = new FileReader();
 
-            reader.onloadend = function(e) {
-                console.log("Text is: "+this.result);
-                document.querySelector("#textArea").innerHTML = this.result;
-            }
+        //     reader.onloadend = function(e) {
+        //         console.log("Text is: "+this.result);
+        //         document.querySelector("#textArea").innerHTML = this.result;
+        //     }
 
-            reader.readAsText(file);
-        });
+        //     reader.readAsText(file);
+        // });
+    },
+    getFile: function(name) { 
+       this.fsroot.getFile(name, null, function(fileEntry) { 
+           fileEntry.file(function(file) { 
+                console.log(file);
+              
+                
+           }, app.fail); 
+       }, app.fail ); 
+    }.bind(this),
+    getFiles: function() { 
+       var dirReader = this.fsroot.createReader(); 
+                console.log(dirReader);
+       dirReader.readEntries(function(files) { 
+            console.log("readEntries");
+            console.log(files);
+           for (var i = 0, len = files.length; i < len; i++) { 
+               if(files[i].isFile) { 
+                   app.getFile(files[i].name); 
+                   console.log(files[i]);
+               } 
+           } 
+       }, app.fail); 
+    }.bind(this),
+
+    createFile: function(path) { 
+    this.fsroot.getFile(path, 
+        { create:true, exclusive:true }, 
+        function() { 
+            console.log(path+ " file created"); 
+        }, 
+        app.fail ); 
+    }.bind(this),
+
+    readFromFile: function(fileName, cb) {
+        var pathToFile = cordova.file.dataDirectory + fileName;
+        window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
+            fileEntry.file(function (file) {
+                var reader = new FileReader();
+
+                reader.onloadend = function (e) {
+                    cb(JSON.parse(this.result));
+                };
+
+                reader.readAsText(file);
+            }, app.errorHandler.bind(null, fileName));
+        }, app.errorHandler.bind(null, fileName));
     },
 
     onDeviceReady: function() {
-     
-        //  //This alias is a read-only pointer to the app itself
-        window.resolveLocalFileSystemURL(cordova.file.dataDirectory,app.gotFile.bind(this), app.fail.bind(this));
-        // // Yes, this works too for our specific example...
-        // $.get("res/sets/fizyka/", function(res) {
-        //     console.log(res);
-        // });
+         cordova.file.options={  
+            basePath: cordova.file.dataDirectory, // working directory
+            create: true, // always create files
+            type: 'text/plain' // read/write all files as plain text
+        };   
+        var fileData;
+        app.readFromFile('/res', function (data) {
+            fileData = data;
+            console.log(fileData);
+        });
         
-    },
+    }.bind(this),
 };
 
 
@@ -62,8 +239,6 @@ var setList = {
         document.getElementById('MainMenuScreen').style.display='none';
         document.getElementById('SetsMenuScreen').style.display='block';
         document.getElementById('SetsMenuBack').addEventListener('click', app.showMain);
-        setList.sets =  setList.iterateFolders();
-        console.log(setList.sets);
 
         setTimeout(function(){  setList.renderTable();}, 10);
     },
@@ -75,8 +250,9 @@ var setList = {
         tbl.className="SetsTable"
         var tbdy = document.createElement('tbody');
 
-        setList.sets.forEach(function(entry) {
+        tempData.forEach(function(entry) {
             console.log(entry);
+            setList.sets.push(entry.object);
         });
 
 
@@ -101,27 +277,6 @@ var setList = {
             tr.appendChild(button);
             tbdy.appendChild(tr);
         });
-        // for (var i = 0; i < 3; i++) {
-        //     var tr = document.createElement('tr');
-        //     var icon = document.createElement('td');
-        //     icon.className="icon";
-        //     icon.innerHTML="icona";
-        //     icon.appendChild(document.createTextNode('\u0020'))
-
-        //     var label = document.createElement('td');
-        //     label.className="label";
-        //     label.innerHTML="nazwa zestawu";
-        //     label.appendChild(document.createTextNode('\u0020'))
-        //     var button = document.createElement('td');
-        //     button.className="button";
-        //     button.innerHTML="X";
-        //     button.appendChild(document.createTextNode('\u0020'))
-
-        //     tr.appendChild(icon);
-        //     tr.appendChild(label);
-        //     tr.appendChild(button);
-        //     tbdy.appendChild(tr);
-        // }
         tbl.appendChild(tbdy);
         body.appendChild(tbl)
     },
