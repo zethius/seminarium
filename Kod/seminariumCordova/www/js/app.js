3e2506124d5d68ca7da63b92555c328f6f1e7ea0
zethius <zethius@mat.umk.pl>
2016-10-20 13:45:57 +0200

    sqlite db

--- Kod/seminariumCordova/www/js/app.js
+++ Kod/seminariumCordova/www/js/app.js
@@ -19,71 +19,57 @@ define(function(require) {
             document.addEventListener('deviceready', this.onDeviceReady, false);
             document.getElementById('MainMenuSetsButton').addEventListener('click', setList.initialize.bind(setList));
             
-        },
+        }.bind(this),
 
         showMain: function(){
             document.getElementById('MainMenuScreen').style.display='block';
             document.getElementById('SetsMenuScreen').style.display='none';
         },
 
-        onDeviceReady: function() {
+      
+
+        prepareDb: function(){
             this.db = window.openDatabase("mnemo", "2.0", "Mnemo DB", 1000000);
-
-
-            // CREATE TABLE IF NOT EXISTS icons(id INTEGER PRIMARY KEY AUTOINCREMENT, icon_value TEXT);
-            // CREATE TABLE IF NOT EXISTS sets(id INTEGER PRIMARY KEY AUTOINCREMENT,
-            //                     description TEXT,
-            //                     difficulty FLOAT,
-            //                     name TEXT,
-            //                     icon_id INTEGER,
-            //                     FOREIGN KEY(icon_id) REFERENCES icons(id)
-            //                    );
-            // CREATE TABLE IF NOT EXISTS colors(id INTEGER PRIMARY KEY AUTOINCREMENT,
-            //                                                      color_value TEXT);
-            // CREATE TABLE IF NOT EXISTS cards(id INTEGER PRIMARY KEY AUTOINCREMENT,
-            //                     front TEXT,
-            //                     back TEXT,
-            //                     difficulty FLOAT,
-            //                     set_id INTEGER,
-            //                     color_id INTEGER,
-            //                     FOREIGN KEY(color_id) REFERENCES colors(id),
-            //                     FOREIGN KEY(set_id) REFERENCES sets(id)
-            //                    );
-
-
 
             this.db.transaction(function(tx) {
                     //create table
-                    console.log("1");
                     tx.executeSql("CREATE TABLE IF NOT EXISTS icons(id INTEGER PRIMARY KEY AUTOINCREMENT, icon_value TEXT)");
-                     console.log("2");
+                    tx.executeSql("DELETE FROM icons");
 
-                     var icons = ['alien.png','atom.png','brain.png',
-                                'compass.png','dna.png', 'earth-globe.png',
-                                'flask.png','gears.png','light-bulb.png',
-                                'pulse.png','rat.png','scientist.png'
+                    var icons = [{'id':1,'val':'alien.png'},
+                                {'id':2,'val': 'atom.png'},
+                                {'id':3,'val':'brain.png'},
+                                {'id':4,'val':'compass.png'},
+                                {'id':5,'val': 'dna.png'}, 
+                                {'id':6,'val': 'earth-globe.png'},
+                                {'id':7,'val': 'flask.png'},
+                                {'id':8,'val': 'gears.png'},
+                                {'id':9,'val': 'light-bulb.png'},
+                                {'id':10,'val': 'pulse.png'},
+                                {'id':11,'val':'rat.png'},
+                                {'id':12,'val':'scientist.png'}
                     ];
-                      console.log("3");
                     for(var i=0; i<icons.length; i++){
-                        // tx.executeSql("INSERT INTO icons(icon_value) VALUES(?)",[icons[i]]);
+                        tx.executeSql("REPLACE INTO icons(id,icon_value) VALUES (?,?)",[icons[i].id, icons[i].val]);
                     }    
-                      console.log("4");
                     tx.executeSql("SELECT * FROM icons",[], function(tx, res){
                                     console.log(res);
                                     // alert(res.rows.item(iii).id);
                                     // alert(res.rows.item(iii).data);
                                     // alert(res.rows.item(iii).data_num);
                             });
-                      console.log("5");
                     tx.executeSql("CREATE TABLE IF NOT EXISTS sets(id INTEGER PRIMARY KEY AUTOINCREMENT,description TEXT,difficulty FLOAT, name TEXT,icon_id INTEGER,FOREIGN KEY(icon_id) REFERENCES icons(id))");
                     tx.executeSql("CREATE TABLE IF NOT EXISTS colors(id INTEGER PRIMARY KEY AUTOINCREMENT,color_value TEXT)");
                     tx.executeSql("CREATE TABLE IF NOT EXISTS cards(id INTEGER PRIMARY KEY AUTOINCREMENT,front TEXT,back TEXT,difficulty FLOAT,set_id INTEGER,color_id INTEGER,FOREIGN KEY(color_id) REFERENCES colors(id),FOREIGN KEY(set_id) REFERENCES sets(id))");
-                      console.log("6");
                 }, function(err){
                     console.log("Error: " + err.message)
+                });  
 
-                });
+        }.bind(this),
 
+        onDeviceReady: function() {
+            console.log(this);
+           this.prepareDb();
         }.bind(this),
     };
     return app;
